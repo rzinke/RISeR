c901059173962be27fad3d9ef295db17c9bf9fad
RobZinke <zink.oxide81@gmail.com>
2019-08-28 12:00:56 -0700

    Added function array2pdf.py and PDFanalysis.py

---
+++ PDFanalysis.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+Find inter-quantile range (IQR) or highest posterior density (HPD)
+of a probability density function (PDF).
+'''
---
+++ array2pdf.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+'''
+Covert an array of numbers into a probability density function (PDF).
+'''
