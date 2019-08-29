5af14781e1155deb9f3a6c48ec1828396bd373e0
Rob - home <zink.oxide81@gmail.com>
2019-08-29 08:31:44 -0700

    Added plotting functions to calcSlipRates. Created new MCresampling script-- empty for now.

---
+++ MCresampling.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Markov chain Monte Carlo sampling schema for Bayesian analysis of fault slip rate data.
+'''
+import numpy as np 
+
+def MCMC_resample(Ages,ageList,Dsps,dspList,condition,maxRate,outName=None):	
+	'''
+	INPUTS
+	OUTPUTS
+	'''
+	pass
+	#return AgePicks, DspPicks, RatePicks
--- calcSlipRates.py
+++ calcSlipRates.py
@@ -4,20 +4,26 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 from SlipRateObjects import *
 
 
 
-## Parser
+### --- Parser ---
 def createParser():
 	import argparse
 	parser = argparse.ArgumentParser(description='Main function for calculating incremental slip rates.')
+	# Required
 	parser.add_argument('-a','--age_list',dest='age_list_file',type=str,required=True,help='Text file with one age file per line, list in order from youngest (top line) to oldest (bottom).')
 	parser.add_argument('-d','--dsp_list',dest='dsp_list_file',type=str,required=True,help='Text file with one displacement file per line, list in order from smallest (top line) to largest (bottom).')
-	parser.add_argument('-o','--output',dest='outName',type=bool,default=False,help='Head name for outputs')
+	parser.add_argument('-o','--output',dest='outName',type=str,required=True,default='Out',help='Head name for outputs (no extension)')
+	# Recommended
+	parser.add_argument('-n','--n_samples',dest='n_samples',type=int,default=1000,help='Number of samples picked in MC run (default 1000; more is often better).')
 	parser.add_argument('-verb','--verbose',dest='verbose',type=bool,default=False,help='Verbose? [True/False]')
-	parser.add_argument('-plot_inputs','--plot_inputs',dest='plot_inputs',type=bool,default=False,help='Plot inputs')
-	parser.add_argument('-plot_outputs','--plot_outputs',dest='plot_outputs',type=bool,default=False,help='Plot outputs')
+	parser.add_argument('-plot_inputs','--plot_inputs',dest='plot_inputs',type=bool,default=False,help='Plot inputs [True/False]')
+	parser.add_argument('-plot_outputs','--plot_outputs',dest='plot_outputs',type=bool,default=False,help='Plot outputs [True/False]')
+	# Highly optional
+	parser.add_argument('-max_picks','--max_picks',dest='max_picks',type=int,default=1000,help='Max number of picks to plot on MC results figure.')
 	return parser 
 
 def cmdParser(inpt_args=None):
@@ -26,7 +32,55 @@ def cmdParser(inpt_args=None):
 
 
 
-## Main
+### --- Ancilary Functions ---
+# Plot raw data (whisker plot)
+def plotRawData(Ages,ageList,Dsps,dspList,xMax,yMax,outName=None):
+	Fraw=plt.figure('RawData')
+	axRaw=Fraw.add_subplot(111)
+	for i in range(m):
+		x_mid=Ages[ageList[i]].median
+		x_err=np.array([[x_mid-Ages[ageList[i]].lowerLimit],
+			[Ages[ageList[i]].upperLimit-x_mid]])
+		y_mid=Dsps[dspList[i]].median
+		y_err=np.array([[y_mid-Dsps[dspList[i]].lowerLimit],
+			[Dsps[dspList[i]].upperLimit-y_mid]])
+		axRaw.errorbar(x_mid,y_mid,xerr=x_err,yerr=y_err,
+			color=(0.3,0.3,0.6),marker='o')
+	axRaw.set_xlim([0,1.1*xMax]) # x-limits
+	axRaw.set_ylim([0,1.1*yMax]) # y-limits
+	axRaw.set_xlabel('age'); axRaw.set_ylabel('offset')
+	axRaw.set_title('Raw data (95 % limits)')
+	if outName:
+		Fraw.savefig('{}_RawData.png'.format(outName),dpi=300)
+	return Fraw,axRaw
+
+# Plot MC results
+def plotMCresults(Ages,ageList,Dsps,dspList,AgePicks,DspPicks, \
+	xMax,yMax,max_picks,outName=None):
+	# Parameters
+	m=len(ageList) # number of data points
+
+	# Plot
+	Fmc=plt.figure('MonteCarlo_results')
+	axMC=Fmc.add_subplot(111)
+	for i in range(m):
+		ageLower=Ages[ageList[i]].lowerLimit # load bottom
+		dspLower=Dsps[dspList[i]].lowerLimit # load left
+		boxWidth=Ages[ageList[i]].upperLimit-ageLower
+		boxHeight=Dsps[dspList[i]].upperLimit-dspLower
+		axMC.add_patch(Rectangle((ageLower,dspLower), # LL corner
+			boxWidth,boxHeight, # dimensions
+			edgecolor=(0.3,0.3,0.6),fill=False,zorder=3))
+	axMC.set_xlim([0,1.1*xMax]); axMC.set_ylim([0,1.1*yMax])
+	axMC.set_xlabel('age'); axMC.set_ylabel('offset')
+	axMC.set_title('MC Picks (N = )')
+	if outName:
+		Fmc.savefig('{}_MCpicks.png'.format(outName),dpi=300)
+	return Fmc,axMC
+
+
+
+### --- Main call function ---
 if __name__ == '__main__':
 	inpt=cmdParser()
 
@@ -105,22 +159,22 @@ if __name__ == '__main__':
 			print('\t{} - {}'.format(ageList[i],dspList[i]))
 
 	## Plot raw data
-	F=plt.figure('RawData')
-	ax=F.add_subplot(111)
-	for i in range(m):
-		x_mid=Ages[ageList[i]].median
-		x_err=np.array([[x_mid-Ages[ageList[i]].lowerLimit],
-			[Ages[ageList[i]].upperLimit-x_mid]])
-		y_mid=Dsps[dspList[i]].median
-		y_err=np.array([[y_mid-Dsps[dspList[i]].lowerLimit],
-			[Dsps[dspList[i]].upperLimit-y_mid]])
-		ax.errorbar(x_mid,y_mid,xerr=x_err,yerr=y_err,
-			color=(0.3,0.3,0.6),marker='o')
-	ax.set_xlim([0,1.1*xmaxGlobal]) # x-limits
-	ax.set_ylim([0,1.1*ymaxGlobal]) # y-limits
-	ax.set_xlabel('age'); ax.set_ylabel('offset')
-	ax.set_title('Raw data')
-	F.savefig('{}_RawData.png'.format(inpt.outName),dpi=300)
+	plotRawData(Ages,ageList,Dsps,dspList,
+		xmaxGlobal,ymaxGlobal,
+		outName=inpt.outName)
+
+	## Monte Carlo resamping
+
+	## Plot MC results
+	plotMCresults(Ages,ageList,Dsps,dspList,
+		None, None,
+		xmaxGlobal,ymaxGlobal,inpt.max_picks,
+		outName=inpt.outName)
+
+	## Convert MC results to PDFs
+
+	## Save data
+
 
 
 	if inpt.plot_inputs==True or inpt.plot_outputs==True:
