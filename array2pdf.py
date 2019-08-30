0da26fc46cd6e84cd6aa0153c1f90af9b4ca8156
Rob - home <zink.oxide81@gmail.com>
2019-08-30 01:20:04 -0700

    added PDF analysis section to calcSlipRates function

--- array2pdf.py
+++ array2pdf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np 
 import matplotlib.pyplot as plt 
+from scipy.stats import gaussian_kde
 from slipRateObjects import gauss
 
 '''
@@ -61,19 +62,23 @@ def arrayHist(V,stepsize,smoothing_kernel=None,kernel_width=2,verbose=False,plot
 			H[0]=0; H[-1]=0
 
 	# Normalize area to 1.0
+	Area=np.trapz(H,Hcntrs) # find area
+	H/=Area # normalize area
+
+	PDF=np.hstack([Hcntrs.reshape(-1,1),H.reshape(-1,1)])
 
 	# Plot if requested
 	if plot is True:
 		F=plt.figure()
 		ax=F.add_subplot(111)
-		ax.plot(Hcntrs,H,'k',linewidth=1)
+		ax.plot(PDF[:,0],PDF[:,1],'k',linewidth=1)
 
-	#return x, px
+	return PDF
 
 
 ## KDE method
 # Convert to PDF using kernel density estimation (inherently smoother)
-def arrayKDE(V,smoothing=None,verbose=False):
+def arrayKDE(V,stepsize,smoothing_kernel=None,kernel_width=2,verbose=False,plot=False):
 	'''
 	INPUTS: 
 		V is an array of values
@@ -82,10 +87,57 @@ def arrayKDE(V,smoothing=None,verbose=False):
 		px is the probability of occurrence
 	'''
 	if verbose is True:
-		print('Converting array to kernel density plot')
+		print('Converting array to histogram')
+		# Compute raw statistics
+		pct=np.percentile(V,[50-95.45/2,50-68.27/2,50,50+68.27/2,50+95.45/2])
+		print('Raw statistics:')
+		print('\tmedian: {0:.3f}'.format(pct[2]))
+		print('\t68.27% range: {0:.3f}-{1:.3f}'.format(pct[1],pct[3]))
+		print('\t95.45% range: {0:.3f}-{1:.3f}'.format(pct[0],pct[4]))
+		print('\tmin: {0:.3f}; max: {1:.3f}'.format(V.min(),V.max()))
 
-	# Compute raw statistics
+	# Independent axis
+	x=np.arange(V.min(),V.max()+stepsize,stepsize)
+
+	# Compute KDE
+	Fkde=gaussian_kde(V) # compute kde function
+	Kde=Fkde(x) # evaluate at axis values
+
+	# Set to zero at edges
+	Kde[0]=0; Kde[-1]=0
+
+	# Smooth if requested
+	if smoothing_kernel:
+		if verbose is True:
+			print('Applying smoothing kernel:\n\ttype: {}\twidth: {}'.format(smoothing_kernel,kernel_width))
+		# Moving mean smoothing
+		if smoothing_kernel.lower() in ['mean']:
+			# Form kernel
+			K=np.ones(kernel_width)
+			# Apply via convolution
+			Kde=np.convolve(Kde,K,'same')
+			# Set ends back to zero
+			Kde[0]=0; Kde[-1]=0
+		elif smoothing_kernel.lower() in ['gauss','gaussian']:
+			# Form kernel
+			kernel_width=kernel_width/2
+			x=np.linspace(-2*kernel_width,2*kernel_width,6*kernel_width) # width of kernel = +- 2 std dev
+			K=gauss(x,0,kernel_width)
+			# Apply via convolution
+			Kde=np.convolve(Kde,K,'same')
+			# Set ends back to zero
+			Kde[0]=0; Kde[-1]=0
 
 	# Normalize area to 1.0
+	Area=np.trapz(Kde,x) # find area
+	Kde/=Area # normalize area
 
-	# return x, px
+	PDF=np.hstack([x.reshape(-1,1),Kde.reshape(-1,1)])
+
+	# Plot if requested
+	if plot is True:
+		F=plt.figure()
+		ax=F.add_subplot(111)
+		ax.plot(PDF[:,0],PDF[:,1],'k',linewidth=1)
+
+	return PDF
