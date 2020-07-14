7552029812e9f806430559fd2bd616374b5cfb7c
RobZinke <zink.oxide81@gmail.com>
2020-07-14 10:42:33 -0700

    Added data plots for ages and displacements examples

---
+++ Examples/ExampleAges/LaunchAgePlot.sh
@@ -0,0 +1,1 @@
+plotAges.py AgeList.txt -x 'ages (ka)' -t 'Sample Examples' -r 10 -o AgePlotExample
---
+++ Examples/ExampleDisplacements/LaunchDispPlot.sh
@@ -0,0 +1,1 @@
+plotDisplacements.py DspList.txt -y 'displacements (m)' -t 'Measurement Examples' -r 80 -o DspPlotExample --generic-color b
