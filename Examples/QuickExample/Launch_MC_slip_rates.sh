5538cf4628faa59f1fedc10c2238f697c2d48e8d
rzinke <zink.oxide81@gmail.com>
2021-01-13 22:01:31 -0800

    Updates to quick example launch script

--- Examples/QuickExample/Launch_MC_slip_rates.sh
+++ Examples/QuickExample/Launch_MC_slip_rates.sh
@@ -12,18 +12,18 @@
 #  Specify distribution to model ages and give parameters in kyrs
 makePDF.py -d gauss -v 4.0 0.8 -o T3T4age
 
-makePDF.py -d gauss -v 6.0 1. -o T2T3age
+makePDF.py -d gauss -v 6.0 1.0 -o T2T3age
 
-makePDF.py -d gauss -v 10. 1. -o T1T2age
+makePDF.py -d gauss -v 10.0 1.0 -o T1T2age
 
 
 # Make displacement PDFs
 #  Specify distribution and give parameters in meters
 makePDF.py -d tri -v 4.0 6.0 9.0 -o T3T4offset
 
-makePDF.py -d trap -v 7.0 8.0 10. 11. -o T2T3offset
+makePDF.py -d trap -v 7.0 8.0 10.0 11.0 -o T2T3offset
 
-makePDF.py -d tri -v 12. 15. 16. -o T1T2offset
+makePDF.py -d tri -v 12.0 15.0 16.0 -o T1T2offset
 
 
 ## CREATE INPUT FILE
@@ -37,17 +37,19 @@ EOM
 
 ## CALCULATE INCREMENTAL SLIP RATES
 Outname=QuickExample  # output head name
-Nsamples=10000    # number of valid samples
+Nsamples=100000   # number of valid samples
+maxRate=100       # maximum slip rate to be considered (e.g., mm/yr)
 PDFmethod=hist    # method to convert sample picks into continuous function
 PDFanalysis=HPD   # report PDF range of values
 SlipRateStep=0.1  # step between slip rate PDF x-axis values
 SmoothType=gauss  # smoothing recommended for histogram of picks
 SmoothWidth=3     # how strong is smoothing
-Confidence=95.45  # percent confidence to report
+Confidence=68.27  # percent confidence to report
 PlotMax=10        # maximum of incr. slip rate plot
 
 calcSlipRates.py Inputs.yaml -o $Outname -n $Nsamples \
---pdf-method $PDFmethod --pdf-analysis $PDFanalysis --rate-step $SlipRateStep \
+--pdf-method $PDFmethod --pdf-analysis $PDFanalysis \
+--max-rate $maxRate --rate-step $SlipRateStep \
 --smoothing-kernel $SmoothType --kernel-width $SmoothWidth \
 --rate-confidence $Confidence --max-rate2plot $PlotMax \
---plot-outputs
+--plot-outputs --verbose
